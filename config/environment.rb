806831fd57ba0407c77687eb6c57beceed331563
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-07-23 08:35:00 +0100

    Dockerfile initialized

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
-require Rails.root.join(Karafka.boot_file)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require Rails.root.join(Karafka.boot_file)
